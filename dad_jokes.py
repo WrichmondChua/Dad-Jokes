a53c7c6bf4927a08e88e78c66019e1f02066c4a6
Wrichmond Well <bangmondcurry@gmail.com>
2025-06-21 21:21:17 +0800

    new project

---
+++ dad_jokes.py
@@ -0,0 +1,6 @@
+import random
+from list_of_dad_jokes import dad_jokes
+
+print(random.choice(dad_jokes))
+
+    
---
+++ list_of_dad_jokes.py
@@ -0,0 +1,13 @@
+#List of dad jokes
+dad_jokes =[
+    "How is my wallet like an onion? Every time I open it, I cry.",
+    "Which vegetable has the best kung fu? Broc-lee.",
+    "I was going to tell a joke about the layoffs, but sadly none of them work.",
+    "Why did the social media manager break up with her boyfriend? Lack of engagement.",
+    "Dad, when he puts the car in reverse: Ah, this takes me back.",
+    "Why did the egg have a day off? Because it was Fryday.",
+    "I made song about tortilla once, now it's more like a wrap.",
+    "My dad told me a joke about boxing. I guess I missed the punch line.",
+    "Where do basketball players go when they need a uniform? New Jersey.",
+    "Why don't football players were glasses? It's a contact sport."
+]
